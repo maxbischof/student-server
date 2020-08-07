80b779e90cdddc44ce016b97bb66bdd149b9a49a
max bischof <max@bischof.dk>
2020-08-07 14:46:35 +0200

    Restructure db.json and server.js

--- server.js
+++ server.js
@@ -13,18 +13,16 @@ server.listen(port, () => {
   console.log(`Server started on port ${port}`)
 })
 
-server.get('/buddys', (req, res) => {
-  res.json(db.buddys)
-})
-
 server.get('/energylevels', (req, res) => {
-  res.json(db.energylevels)
+    console.log('got get')
+    console.log(db.students.find(student => student.id == req.query.id))
+    res.json(db.students.find(student => student.id == req.query.id))
+    //res.end('jo')
 })
 
 server.post('/energylevels', (req, res) => {
   console.log('working')
   db.energylevels.push(req.body)
   res.statusCode = 200
-  res.end('Data received')
-  console.log(db.energylevels)
+  res.end(req.body)
 })
